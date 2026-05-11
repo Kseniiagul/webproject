ffaa59b6464eaad5767e607338f40fa5e5a9d5ef
Alena12223 <abikova135@gmail.com>
2026-05-11 14:42:46 +0300

    Create view.py

---
+++ routes/view.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, render_template, abort, session
+from database.notes import Note
+from database.attachments import Attachment
+from database.db_session import create_session
+from utils.md_parser import to_html
+
+blueprint = Blueprint('view', __name__, templates_folder='templates')
+
+@blueprint.route('/note/<int:note_id>', methods=['GET'])
+def view_note(note_id):
+    if 'user_id' not in session:
+        abort(401)
+
+    db_sess = create_session()
+    note = db_sess.query(Note).get(note_id)
+    if not note or note.user_id != session['user_id']:
+        db_sess.close()
+        abort(404)
+
+    rendered_html = to_html(note.content)
+
+    attachments = db_sess.query(Attachment).filter(Attachment.note_id == note_id).all()
+
+    db_sess.close()
+
+    return render_template('note_view.html', note=note, rendered_html=rendered_html, attachments=attachments)
