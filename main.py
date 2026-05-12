2df28a3ab8c8867484591f376e125709e1463be2
arina-minina <aroma.minina.serg@gmail.com>
2026-05-12 20:07:27 +0300

    перенос маршрутов для авторизации в отдельный файл (освобождение main)

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from datetime import datetime
-from forms.user import RegisterForm, LoginForm
-from database.users import User
 from database import db_session
-from flask import render_template, redirect, Flask
-
-from flask_login import LoginManager, login_user, login_required, logout_user
-
+from flask import Flask
+from routes.auth import authorization_routes
 from routes.export import export_note
 from routes.view import view_note
 from routes.api import api_note
@@ -13,60 +8,7 @@ from routes.api import api_note
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'our-secret-key-here'
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    db_sess = db_session.create_session()
-    return db_sess.get(User, user_id)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.username == form.username.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return render_template('login.html',
-                               message="Неправильный логин или пароль",
-                               form=form)
-    return render_template('login.html', title='Авторизация', form=form)
-
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect("/")
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def reqister():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Пароли не совпадают")
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.username == form.username.data).first():
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Такой пользователь уже есть")
-        user = User(
-            username=form.username.data,
-            created_at=datetime.now()
-        )
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-        return redirect('/login')
-    return render_template('register.html', title='Регистрация', form=form)
+authorization_routes(app)
 
 
 def main():
